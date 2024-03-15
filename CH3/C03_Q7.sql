37151f9616f8f381f67212a56c6670a28fded201
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2024-03-15 11:17:05 +0800

    Create C03_Q7.sql

---
+++ CH3/C03_Q7.sql
@@ -0,0 +1,9 @@
+SELECT shohin_catalg,COUNT(*) FROM shohin
+GROUP BY buying_price;
+-- shohin_catalg COUNT(*)
+-- 廚房用品        2
+-- 辦公用品        1
+-- 衣物            1
+-- 廚房用品        1
+-- 衣物            2
+-- 廚房用品        1
