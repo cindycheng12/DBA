7dc24022f3c609f8cd68bb574ba205279bd3257b
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2024-05-16 16:27:33 +0800

    !!!!Create C06_03.sql

---
+++ CH06/C06_03.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Orders 
+(
+OrderId int NOT NULL,
+ProductName varchar(50) NOT NULL,
+OrderDate datetime NOT NULL DEFAULT CURDATE(),
+PRIMARY KEY (OrderId)
+)
+
+
+
+待改
+?????INSERT INTO orders (ProductName, OrderDate) VALUES ('Computer', NOW());
