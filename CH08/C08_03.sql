55d5a58532709be69ad15e050f6aafe1ead74052
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2024-06-12 22:30:57 +0800

    Update C08_03.sql

--- CH08/C08_03.sql
+++ CH08/C08_03.sql
@@ -1,4 +1,4 @@
-SELECT shohin_name,shohin_catalg,sell_price, 
+SELECT shohin_id,shohin_name,sell_price, 
 RANK() OVER (ORDER BY sell_price) AS ranking, 
 DENSE_RANK() OVER (ORDER BY sell_price)AS dense_ranking, 
 ROW_NUMBER() OVER(ORDER BY sell_price) AS row_num 
